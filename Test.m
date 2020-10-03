7e6f07db3e19321366096dd6a1ff801a91d4fc06
Wei Liu <weiliusjtu@gmail.com>
2020-10-03 10:38:42 +0800

    updated by Wei Liu

--- Test.m
+++ Test.m
@@ -16,3 +16,13 @@ ImgE = Img + 3 * Diff;
 figure; imshow(Smoothed)
 figure; imshow(ImgE)
 
+%%
+Img = im2double(imread('clip_art.jpg'));
+lambda = 30;
+gamma = 5/255
+iter = 10;
+
+Smoothed = ILS_Welsch(Img, lambda, iter);
+
+figure; imshow(Img)
+figure; imshow(Smoothed)
